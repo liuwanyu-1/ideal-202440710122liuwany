d4f25c33f0de295b45f8e6e56168ab9fc728b21f
liuwanyu-1 <2891619545@qq.com>
2026-09-16 23:54:35 +0800

    Add src/main/java/org/example/ch2/q9/DouDiZhuPoker.java

---
+++ src/main/java/org/example/ch2/q9/DouDiZhuPoker.java
@@ -0,0 +1,15 @@
+package org.example.ch2.q9;
+
+import java.util.List;
+
+public class DouDiZhuPoker extends Poker {
+    @Override
+    public List<String>[] deal() {
+        return super.deal();
+    }
+
+    @Override
+    public void show(List<String> list) {
+        super.show(list);
+    }
+}
